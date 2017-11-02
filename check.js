a498a5a63c28c8c73f82a4e852a2e7ca6f30d4a7
babin78 <arijit.roy@newgen.co.in>
2017-11-03 01:38:11 +0530

    chek-validate format before path

--- check.js
+++ check.js
@@ -8,12 +8,12 @@ try{
   				  //{workstepname:'work intrudoction' , state:'start',rules:['rule1']},
             //{workstepname:'work intrudoction' , state:'start',rules:['rule2']},
             //{workstepname:'work intrudoction', STate:'STart',rules:[""]},  //
-            {workstepname:'Work intrudoction', STATE:'Start',rules:[""]},
+            {workstepname:'Work intrudoction', STATE:'Start',rules:["rule1"]},
             {workstepname:'decision1', state:'decision',rules:['rule3','rule4','rule5','rule15']},
   				  {workstepname:'discard', state:'end',rules:'NULL'},
   				  {workstepname:'exit', state:'end',rules:'NULL'},
   				  {workstepname:'data-entry', state:'custom',rules:['rule2']},
-  				  {workstepname:'ckecher', state:'custom',rules:['rule6']},
+  				  {workstepname:'checker', state:'custom',rules:['rule6']},
   				  {workstepname:'decision2', state:'decision',rules:['rule7','rule8','rule9']},
   				  {workstepname:'split', state:'split',rules:['rule10']},
   				  {workstepname:'join', state:'join',rules:['rule11']},
@@ -24,22 +24,23 @@ try{
   			  ]
 
 var          rulebook=[
-          				 {rulename:'rule1',type:'auto',to:'data entry' },
+
+          				 {rulename:'rule1',type:'auto',to:'data-entry' },
           				 {rulename:'rule2',type:'auto',to:'decision1'},
           				 {rulename:'rule3',type:'decision',condition:{ $and:[ {$gt:{amount:100}},{acceptflag:'accept'}]}, to:'checker'},
                    {rulename:'rule15',type:'decision',condition:{acceptflag:'accept'}, to:'split'},
                    {rulename:'rule4',type:'decision',condition: {discardflag:'discard'},to:'discard'},
-          				 {rulename:'rule5',type:'decision',condition: 'else', to:'datanetry'},
+          				 {rulename:'rule5',type:'decision',condition: 'else', to:'data-entry'},
 
           				 {rulename:'rule6',type:'auto',to:'decision2'},
           				 {rulename:'rule7',type:'decision',condition:{acceptflag:'accept'}, to:'split'},
           				 {rulename:'rule8',type:'decision',condition: {discardflag:'discard'}, to:'discard'},
           				 {rulename:'rule9',type:'decision',condition: 'else', to:'checker'},
 
-          				 {rulename:'rule10',type:'split',to:'techvieryfy'},
-          				 {rulename:'rule11',type:'split',to:'filegen'},
+          				 {rulename:'rule10',type:'split',to:'tech-verify'},
+          				 {rulename:'rule11',type:'split',to:'filegenerate'},
           				 {rulename:'rule12',type:'auto',to:'join'},
-          				 {rulename:'rule13',type:'auto',to:'read-response'},
+          				 {rulename:'rule13',type:'auto',to:'readresponse'},
           				 {rulename:'rule14',type:'auto',to:'join'},
           				 {rulename:'rule16',type:'auto',to:'exit'}
 
@@ -47,12 +48,12 @@ var          rulebook=[
 
 
 var Str=JSON.stringify(queues)
-var lowerStr=Str.toLowerCase()
+var lowerStr=Str.toLowerCase(Str)
 var lowerObj=JSON.parse(lowerStr)
  queues=lowerObj
 
  Str=JSON.stringify(rulebook)
- lowerStr=Str.toLowerCase()
+ lowerStr=Str.toLowerCase(Str)
  lowerObj=JSON.parse(lowerStr)
   rulebook=lowerObj
 //console.log(lowerObj)
@@ -61,6 +62,40 @@ catch(e){
   console.log(e)
 
 }
+
+
+
+var allworkstepnames=_.map(queues,v=>{return v.workstepname})
+
+var allruletonames=_.map(rulebook,v=>{return v.to})
+var allruleuniquetonames=_.unique(allruletonames)
+var isAllRuleToFound=true
+_.each(allruleuniquetonames,v=>{
+  if(!_.contains(allworkstepnames,v))
+  {
+    console.log(v)
+    isAllRuleToFound=false
+  }
+})
+if(!isAllRuleToFound)
+console.log('some rule [to] field value is not found in queues/workstep')
+
+
+var allrulesinQ=_.filter(_.flatten(_.map(queues,v=>{return v.rules})),v=>{  return v != 'null'})
+//console.log(allrulesinQ)
+var allruleuniquenames =_.unique( _.map(rulebook,v=>{return v.rulename}))
+var isAllQueueRulematched=true
+_.each(allrulesinQ,v=>{
+  if(!_.contains(allruleuniquenames,v))
+  {
+    console.log(v)
+    isAllQueueRulematched=false
+  }
+})
+if(!isAllQueueRulematched)
+console.log('some rules in queus dont match with rulename in rulebook')
+
+
 
 //queues=""
         if (!_.isArray(queues) || (_.isArray(queues) &&  queues.length==0 ))
@@ -72,7 +107,7 @@ catch(e){
         //console.log(_.isArray(queues) && queues.length>0  )
 
         var dupq=(_.find(gr,v=>{return v.length>1}))
-        console.log(dupq)
+        //console.log(dupq)
         if(dupq)
         {   console.log('queue has duplicate entry')
             process.exit(0)
@@ -122,7 +157,7 @@ catch(e){
        console.log('invalid state')
 
 
-       if (!_.isArray(rulebook) || (_.isArray(rulebook) &&  queues.length==0 ))
+       if (!_.isArray(rulebook) || (_.isArray(rulebook) &&  rulebook.length==0 ))
        {   console.log('rulebook is not an valid item')
            process.exit(0)
        }
@@ -139,22 +174,45 @@ catch(e){
 
 
        var isAllRulePropertyFound=true
-       var isAllRuleTypeFound
+       var isAllRuleTypeFound=true
        var isAllhaveRulesR
 
 
        _.each(rulebook,r=>{
+
+
          if(!(_.has(r, "rulename") && _.has(r, "type")  ))
            { console.log(r)
              isAllRulePropertyFound=false
            }
 
+           if(r.type=="decision" && (!_.has(r, "condition")))
+           {
+             console.log(r)
+               isAllRulePropertyFound=false
+
+           }
+
+           if(r.type=="decision" && _.has(r, "condition"))
+           {
+               if((r.condition!=="else" ) && (typeof(r.condition)!=='object'))
+               {
+                 console.log(r.decision)
+                 isAllRulePropertyFound=false
+               }
 
 
+           }
            var ruletypevals=["auto","decision","split","join","set","expire"]
            if( _.has(r, "type") && (!_.contains(ruletypevals,r.type)))
-           isValidState=false
-
+           {
+             console.log(r)
+             isAllRuleTypeFound=false
+          }
              //check each rule entry should be in rulebook array
 
        })
+       if(!isAllRulePropertyFound)
+       console.log('all rules dont  have either type/rulename')
+       if(!isAllRuleTypeFound)
+       console.log('all rules dont  have valid type')
